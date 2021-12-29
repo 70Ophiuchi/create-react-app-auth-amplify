d9ab64f8409905e6adb15f486435f82d7a4d6aa3
PWNEDJonesy <PWNEDJonesy@users.noreply.github.com>
2021-12-29 20:23:26 +0500

    commit to master

--- src/App.js
+++ src/App.js
@@ -7,27 +7,89 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+	state = {
+
+    selectedFile: null
+    };
+    
+    onFileChange = event => {
+    
+    this.setState({ selectedFile: event.target.files[0] });
+    
+    };
+    
+    onFileUpload = () => {
+    
+    const formData = new FormData();
+    
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+    
+    console.log(this.state.selectedFile);
+    
+    fetch(
+			'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+    };
+    
+    fileData = () => {
+    
+    if (this.state.selectedFile) {
+      
+      return (
+      <div>
+        <h2>File Details:</h2>
+        
+  <p>File Name: {this.state.selectedFile.name}</p>
+  
+        
+  <p>File Type: {this.state.selectedFile.type}</p>
+  
+        
+  <p>
+        Last Modified:{" "}
+        {this.state.selectedFile.lastModifiedDate.toDateString()}
+        </p>
+  
+      </div>
+      );
+    } else {
+      return (
+      <div>
+        <br />
+        <h4>Choose before Pressing the Upload button</h4>
+      </div>
+      );
+    }
+    };
+  
   render() {
     return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+        <h1>
+            File Upload
+        </h1>
+        <h3>
+            TEMPLATE HTML FILE
+        </h3>
+        <div>
+            <input type="file" onChange={this.onFileChange} />
+            <button onClick={this.onFileUpload}>
+            Upload!
+            </button>
+        </div>
+        {this.fileData()}
+    </div>
+    )
   }
 }
 
 export default withAuthenticator(App);
+

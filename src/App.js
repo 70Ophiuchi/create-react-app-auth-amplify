66eb0f2c5ce006f7c8510ed944b6faddfdf63b39
PWNEDJonesy <PWNEDJonesy@users.noreply.github.com>
2021-12-31 06:40:58 +0500

    commit to master

--- src/App.js
+++ src/App.js
@@ -6,62 +6,6 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-/**
- * Helper function for POSTing data as JSON with fetch.
- *
- * @param {Object} options
- * @param {string} options.url - URL to POST data to
- * @param {FormData} options.formData - `FormData` instance
- * @return {Object} - Response body from URL that was POSTed to
- */
- async function postFormDataAsJson({ url, formData }) {
-	const plainFormData = Object.fromEntries(formData.entries());
-	const formDataJsonString = JSON.stringify(plainFormData);
-
-	const fetchOptions = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json",
-		},
-		body: formDataJsonString,
-	};
-
-	const response = await fetch(url, fetchOptions);
-
-	if (!response.ok) {
-		const errorMessage = await response.text();
-		throw new Error(errorMessage);
-	}
-
-	return response.json();
-}
-
-/**
- * Event handler for a form submit event.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
- *
- * @param {SubmitEvent} event
- */
-async function handleFormSubmit(event) {
-	event.preventDefault();
-
-	const form = event.currentTarget;
-	const url = form.action;
-
-	try {
-		const formData = new FormData(form);
-		const responseData = await postFormDataAsJson({ url, formData });
-
-		console.log({ responseData });
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-const exampleForm = document.getElementById("post-form");
-exampleForm.addEventListener("submit", handleFormSubmit);
 
 class App extends Component {
 
@@ -147,7 +91,7 @@ class App extends Component {
         </div>
         {this.fileData()}
 
-        <form action="https://qqrpo8v6n6.execute-api.eu-central-1.amazonaws.com/default/SES_TEST_EMAIL" method="POST" target='blank' id='post-form'>
+        <form action="https://qqrpo8v6n6.execute-api.eu-central-1.amazonaws.com/default/SES_TEST_EMAIL" method="POST" target='blank'>
 
             <label for="TemplateName">Template Name:</label>
             <input type="text" name="TemplateName" placeholder="Template Name"/> <br /><br />
